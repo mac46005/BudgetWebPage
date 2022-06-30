2609023e2407bdcac47e34c8452ce5e07b135a54
Marco Preciado <mac46005@gmail.com>
2022-06-30 13:44:49 -0500

    created expensesubtype class

---
+++ dataManager/expenses/expenseSubTypeDataManager.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+
+    
+    ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expense Sub Type Data Manager</title>
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/components/_nav.css">
+    <link rel="stylesheet" href="../../css/components/_dashboard.css">
+
+</head>
+<body>
+    <nav>
+        <div class="nav-container">
+            <h4><a href="../../dashBoard.php">BudgetApp</a></h4>
+
+            <ul class="nav-menu">
+                <li><a href="../DataManagerHome.html">Manage Types</a></li>
+                <li><a href="#">Mng Income</a></li>
+                <li><a href="#">Mng Expense</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <header>
+        <div class="header-container">
+            <h2>
+                Expense Sub Type Data Manager
+            </h2>
+        </div>
+    </header>
+</body>
+</html>
---
+++ script/php/dbAccess/models/expenseSubType.php
@@ -0,0 +1,30 @@
+<?php
+class ExpenseSubType{
+    public $id = 0;
+    public $name = "";
+    public $expenseType_id = 0;
+    public $dateCreated = "";
+    public $dateModified = "";
+
+    function __construct($id = 0, $name = "",$expenseType_id = 0, $dateCreated = "", $dateModified = "")
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->expenseType_id = $expenseType_id;
+        $this->dateCreated = $dateCreated;
+        $this->dateModified = $dateModified;
+    }
+
+    function __toString()
+    {
+        $toString = <<<STRING
+        id: $this->id<br/>
+        name: $this->name<br/>
+        expenseType_id: $this->expenseType_id<br/>
+        dateCreated: $this->dateCreated<br/>
+        dateModified: $this->dateModified<br/>
+        STRING;
+        return $toString;
+    }
+}
+?>
